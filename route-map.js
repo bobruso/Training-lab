78aad2e70a98e813a18f933fbe10d2a0c4fee647
bobruso <51798343+bobruso@users.noreply.github.com>
2026-09-11 06:47:23 +0200

    Allow reanalysis of stored FIT activities

--- route-map.js
+++ route-map.js
@@ -1,5 +1,6 @@
 import './activity-detail-enhancer.js?v=20260911insights86';
 import './activity-sport-panels.js?v=20260911sports88';
+import './activity-fit-reanalysis.js?v=20260911sports89';
 
 export const MAP_PROVIDER={
  tileSize:256,
