da418124da6f01d1a106ed56bf92945fb069ce5a
bobruso <51798343+bobruso@users.noreply.github.com>
2026-09-11 16:53:24 +0200

    Bump companion to 0.9

--- android-companion/app/build.gradle.kts
+++ android-companion/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.traininglab.companion"
         minSdk = 28
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.8"
+        versionCode = 9
+        versionName = "0.9"
     }
 }
 dependencies {
